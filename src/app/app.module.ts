0f7135a6e539c80e504276fade57846835c7afed
Naveen Padigala <n_padigala@apple.com>
2020-04-27 01:36:44 +0530

    formResult component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FormInputComponent } from './form-input/form-input.component';
+import { FormResultComponent } from './form-result/form-result.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FormInputComponent
+    FormInputComponent,
+    FormResultComponent
   ],
   imports: [
     BrowserModule,
